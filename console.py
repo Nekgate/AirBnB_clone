c0293cf343c73563b454d9537af1418781d002f6
Nekgate <Nekgate@gmail.com>
2024-02-08 11:14:10 +0100

    Module for console that contains the entry point of command interpreter

--- console.py
+++ console.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" Module for consule that contains the entry point of command interpreter """
+""" Module for console that contains the entry point of command interpreter """
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
-    
+
     def do_quit(self, line):
         """ Quit command to exit program """
         print("Good Bye")
@@ -13,7 +14,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, line):
         """ Exit the program when EOF (Ctrl+D) is entered """
-        print() #  Print a newline before exiting
+        print()  # Print a newline before exiting
         return True
 
     def help_quit(self):
@@ -23,6 +24,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Overwrite the empty line """
         return False
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
