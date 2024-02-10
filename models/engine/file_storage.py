20ca3bf53641fcb06dccf85c2a60b22ca6a5d2d2
Iamouma <stephenouma452@gmail.com>
2024-02-10 13:04:36 -0800

    File

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,6 @@ from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
-B
 from models.amenity import Amenity
 from models.review import Review
 
